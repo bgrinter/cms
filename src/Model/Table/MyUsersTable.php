faadab85f5132b50d804a03b371c0c26b0be2443
Brian Grinter <bgrinter@gmail.com>
2022-02-19 21:41:52 +1100

    Fix for "is_active" field and customer finder

--- src/Model/Table/MyUsersTable.php
+++ src/Model/Table/MyUsersTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Table;
 
+use Cake\ORM\Query;
 use CakeDC\Users\Model\Table\UsersTable;
 
 /**
@@ -10,4 +11,17 @@ use CakeDC\Users\Model\Table\UsersTable;
 class MyUsersTable extends UsersTable
 {
 
+
+    /**
+     * Custom finder to filter active users
+     *
+     * @param \Cake\ORM\Query $query Query object to modify
+     * @return \Cake\ORM\Query
+     */
+    public function findActive(Query $query)
+    {
+        $query->where([$this->aliasField('is_active') => 1]);
+
+        return $query;
+    }
 }
